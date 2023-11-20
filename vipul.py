5ab5dc5e7e02ce18bbc6b78d3c74108df3588208
its-hacker-27 <choudharyvipul896@gmail.com>
2023-11-21 01:00:12 +0530

    second commit

---
+++ vipul.py
@@ -0,0 +1,86 @@
+#name = "vipul"
+#age = 22
+#print(name)
+#age = "23"
+#print(age)
+# = "Tony stark"
+#is_adult
+#name = input("What is your name")
+#print("Hello " + name)
+#old_age = input('enter your current age')
+#new_age=(int(old_age)*2) + 6
+#print(new_age)
+#first = input("Enter 1st number")
+#sum = int(first) + int(second)
+#print("the sum is:"  + str(sum))
+#name = "vipul"
+#print(name)
+#age = "345"
+
+#i = 5
+#while i >= 0:
+    #print(i * "*")
+    #i = i - 1
+#for item in range(6):
+  # print(item + 1)
+#marks = [34,43,67]
+#print(marks[2],marks[1])
+#marks = [234,432,233]
+#print(marks[-3])
+#marks = [23,34,45]
+#marks.insert(1,56)
+#print(marks)
+#students = ["ram","shyam","harshit","nitish","vipin"]
+
+#for jelly in students:
+   # print(jelly)
+    #if jelly=="nitish":
+      #  break;
+#marks = {"english" : 95, "chemistry": 98}
+#print(marks['chemistry'])
+#print(marks['english'])
+#from math import sqrt
+#print(sqrt(36))
+''''
+def print_sum(first,second):
+    #print(first + second)
+
+print_sum(56,45)
+print("hello i am a ggod boy\n'[--]`and you are a abd gir
+'''
+#print("Hey",6,7, sep='~')
+#a= 45
+#print(a+b)
+#x = input("Enter the 1st number:")
+#y = input("Enter 2nd number:")
+#print(int(x)+int(y))
+#name = "HarrefasdgyhdC.Jdahk.jEQFBdbvdjbcdkhb"
+#print("hello" + name)
+
+#print("Lets use a loop\n")
+#for character in apple:
+#for character in name:
+    #print(character)
+#names = "harry,shubham,pawan"
+#print(names[0:6])
+'''''
+fruit = "Mango"
+mangolen = len(fruit)
+print(mangolen)
+print(fruit[0:3])
+print(fruit[0:4])
+print(fruit[-3:-1])
+print(fruit[1:3])
+'''
+'''''
+name = "harry"
+for code in name:
+    print(code)
+'''
+a = "Harry Harry knwknfke hgjkhw HarryHarry"
+#print(a.upper())
+#print(a.lower())
+##print(a.split(" "))
+print(a.count("Harry"))
+
+
